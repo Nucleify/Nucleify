9122e38eb44560a353564f32c6186e966a1ff973
SzymCode <szymon.radomski@yahoo.com>
2024-11-18 13:11:37 +0100

    🛠️ Workflow: require_once instead of require

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,10 @@
 <?php
 
 // USE THIS IF YOU RUN TESTS ON LINUX
-require __DIR__ . '/TestExpectations.php';
-require __DIR__ . '/TestFunctions.php';
-require __DIR__ . '/TestGroups.php';
-require __DIR__ . '/TestUses.php';
+require_once __DIR__ . '/TestExpectations.php';
+require_once __DIR__ . '/TestFunctions.php';
+require_once __DIR__ . '/TestGroups.php';
+require_once __DIR__ . '/TestUses.php';
 
 $articleData = require_once __DIR__ . '/TestConstants.php';
 $updatedArticleData = require_once __DIR__ . '/TestConstants.php';
@@ -15,10 +15,10 @@ $updatedUserData = require_once __DIR__ . '/TestConstants.php';
 
 
 // USE THIS IF YOU RUN TESTS ON WINDOWS
-// require __DIR__ . '\TestExpectations.php';
-// require __DIR__ . '\TestFunctions.php';
-// require __DIR__ . '\TestGroups.php';
-// require __DIR__ . '\TestUses.php';
+// require_once __DIR__ . '\TestExpectations.php';
+// require_once __DIR__ . '\TestFunctions.php';
+// require_once __DIR__ . '\TestGroups.php';
+// require_once __DIR__ . '\TestUses.php';
 
 // $articleData = require_once __DIR__ . '\TestConstants.php';
 // $updatedArticleData = require_once __DIR__ . '\TestConstants.php';
