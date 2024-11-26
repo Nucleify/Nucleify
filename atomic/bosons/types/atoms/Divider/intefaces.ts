15384d525670e189079a1fa3dbe409d94d1ab60c
KatarzynaS97 <katarzyna33445@wp.pl>
2024-11-26 22:10:42 +0100

    ⚛ Atomic Design: Updated props for PrimeVue Divider

--- atomic/bosons/types/atoms/Divider/intefaces.ts
+++ atomic/bosons/types/atoms/Divider/intefaces.ts
@@ -1,14 +1,14 @@
 import { PassThrough } from 'primevue/ts-helpers'
 import { DividerPassThroughOptions } from 'primevue/divider'
-import { PassThroughOptions } from 'primevue/passthrough'
 
 import { ElementDirectionType, PositionType } from 'atomic/bosons/types'
 
 export interface DividerInterface {
   align?: PositionType | 'center'
   layout?: ElementDirectionType
-  type?: 'dashed' | 'dotted' | 'solid'
+  type?: 'solid' | 'dashed' | 'dotted'
+  dt?: any // eslint-disable-line @typescript-eslint/no-explicit-any
   pt?: PassThrough<DividerPassThroughOptions>
-  ptOptions?: PassThroughOptions
+  ptOptions?: any // eslint-disable-line @typescript-eslint/no-explicit-any
   unstyled?: boolean
 }
