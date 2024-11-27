414447aca528f5d9c0d439090bd2c691092f8651
KatarzynaS97 <katarzyna33445@wp.pl>
2024-11-27 22:25:20 +0100

    ⚛ Atomic Design: Updated props for PrimeVue RadioButton

--- atomic/bosons/types/atoms/RadioButton/interfaces.ts
+++ atomic/bosons/types/atoms/RadioButton/interfaces.ts
@@ -1,14 +1,19 @@
 import { PassThrough } from 'primevue/ts-helpers'
 import { RadioButtonPassThroughOptions } from 'primevue/radiobutton'
-import { PassThroughOptions } from 'primevue/passthrough'
 
-import { AdTypeType, ElementVariantType } from 'atomic/bosons/types'
+import {
+  AdTypeType,
+  ElementSizeType,
+  ElementVariantType,
+} from 'atomic/bosons/types'
 
 export interface RadioButtonInterface {
   adType?: AdTypeType
   value?: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  defaultValue?: any // eslint-disable-line @typescript-eslint/no-explicit-any
   name?: string
   binary?: boolean
+  size?: ElementSizeType
   invalid?: boolean
   disabled?: boolean
   variant?: ElementVariantType
@@ -19,7 +24,9 @@ export interface RadioButtonInterface {
   inputClass?: string
   ariaLabelledby?: string
   ariaLabel?: string
+  formControl?: Record<string, any> // eslint-disable-line @typescript-eslint/no-explicit-any
+  dt?: any // eslint-disable-line @typescript-eslint/no-explicit-any
   pt?: PassThrough<RadioButtonPassThroughOptions>
-  ptOptions?: PassThroughOptions
+  ptOptions?: any // eslint-disable-line @typescript-eslint/no-explicit-any
   unstyled?: boolean
 }
