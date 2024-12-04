21b27b7fbb7d25237074853a5e7d4fd47ff9bba1
SzymCode <szymon.radomski@yahoo.com>
2024-12-04 21:23:45 +0100

    Storybook: Move Storybook's package.json and Dockerfile up to the project root folder
    Example story
    
    Corrections

---
+++ .storybook/preview.ts
@@ -0,0 +1,48 @@
+
+
+import '../atomic/bosons/styles/index.scss'
+
+/**
+ *  App mount
+ */
+import { createApp } from 'vue'
+export const app = createApp({})
+
+/**
+ *  PrimeVue
+ */
+import '../atomic/bosons/styles/index.scss'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import PrimeVue from 'primevue/config'
+
+app.use(PrimeVue, {
+  theme: {
+    preset: DataManagerPreset,
+    options: {
+      darkModeSelector: true,
+    },
+  },
+  ripple: true,
+})
+
+import { DataManagerPreset } from '../atomic/primevue_preset'
+import type { Preview } from '@storybook/vue3'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
+
+/**
+ *  Use DataManager's Sass styles
+ */
--- storybook/src/templates/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  Templates
- */
-export { default as ButtonTemplate } from './Button'
-export { default as InputTextTemplate } from './InputText'
-export { default as ProgressSpinnerTemplate } from './ProgressSpinner'
-export { default as TextAreaTemplate } from './TextArea'
