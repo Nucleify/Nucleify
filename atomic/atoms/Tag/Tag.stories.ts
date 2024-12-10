13a81b3b04d7a9eb45d21d649cd752b38960d25a
KatarzynaS97 <katarzyna33445@wp.pl>
2024-12-10 18:01:34 +0100

    Storybook: Story for Tag

---
+++ atomic/atoms/Tag/Tag.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import { Tag as AdTag } from './'
+
+const meta = {
+  title: 'Example/Tag',
+  component: AdTag,
+  tags: ['autodocs'],
+  args: {
+    value: 'Text',
+    severity: 'success',
+    rounded: false,
+    icon: '',
+    dt: {},
+    pt: {},
+    ptOptions: {},
+    unstyled: false,
+  },
+  argTypes: {
+    value: { control: 'text' },
+    severity: {
+      control: 'select',
+      options: [
+        'success',
+        'secondary',
+        'info',
+        'warning',
+        'danger',
+        'contrast',
+      ],
+    },
+  },
+} satisfies Meta<typeof AdTag>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Tag: Story = {}
--- atomic/atoms/Tag/index.vue
+++ atomic/atoms/Tag/index.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script setup lang="ts">
+import Tag from 'primevue/tag' // Import for AvatarStories
+
 import { TagInterface } from 'atomic/bosons/types'
 
 const props = defineProps<TagInterface>()
