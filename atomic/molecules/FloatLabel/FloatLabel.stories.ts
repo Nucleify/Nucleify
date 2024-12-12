2fb38633c6ec9759cf0f6c6dbce8c086700d2a6e
KatarzynaS97 <katarzyna33445@wp.pl>
2024-12-12 10:32:18 +0100

    Storybook: Story for FloatLabel

---
+++ atomic/molecules/FloatLabel/FloatLabel.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import { FloatLabel as AdFloatLabel } from './'
+
+const meta = {
+  title: 'Example/FloatLabel',
+  component: AdFloatLabel,
+  tags: ['autodocs'],
+  args: {
+    dt: {},
+    pt: {},
+    ptOptions: {},
+    unstyled: false,
+    variant: 'in',
+  },
+  argTypes: {
+    variant: { control: 'select', options: ['in', 'over', 'on'] },
+  },
+} satisfies Meta<typeof AdFloatLabel>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const FloatLabel: Story = {}
