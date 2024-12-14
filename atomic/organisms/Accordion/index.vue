99365d935906b9eb1cc90a142b8630947717efa7
KatarzynaS97 <katarzyna33445@wp.pl>
2024-12-14 14:58:15 +0100

    Storybook: Story for Accordion

--- atomic/organisms/Accordion/index.vue
+++ atomic/organisms/Accordion/index.vue
@@ -24,6 +24,11 @@
 </template>
 
 <script setup lang="ts">
+import Accordion from 'primevue/accordion' // Import for  Stories
+import AccordionPanel from 'primevue/accordionpanel' // Import for  Stories
+import AccordionContent from 'primevue/accordioncontent' // Import for  Stories
+import AccordionHeader from 'primevue/accordionheader' // Import for  Stories
+
 import { AccordionInterface } from 'atomic/bosons/types'
 
 const props = defineProps<AccordionInterface>()
