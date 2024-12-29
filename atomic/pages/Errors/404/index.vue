15b3ee033173004c61130fe463398b158eac1a3f
szymcode <szymon.radomski@yahoo.com>
2024-12-29 01:24:44 +0100

    Changed error 404 animation

--- atomic/pages/Errors/404/index.vue
+++ atomic/pages/Errors/404/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="error-404-container container">
-    <error404-animation-first />
+    <error404-animation-second />
     <ad-anchor href="/" label="Got lost?" class="mt-4" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Error404AnimationFirst } from './animations'
+import { Error404AnimationSecond } from './animations'
 </script>
