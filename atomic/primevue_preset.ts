cafb7c3a81d5156e1b272a49c4e3e852072b33a7
szymcode <szymon.radomski@yahoo.com>
2025-01-07 21:07:29 +0100

    ⚛ Atomic Design: Removed colorScheme from primevue_preset.ts - we'll use only dark mode

--- atomic/primevue_preset.ts
+++ atomic/primevue_preset.ts
@@ -4,104 +4,60 @@ import Lara from '@primevue/themes/lara'
 export const DataManagerPreset = definePreset(Lara, {
   components: {
     card: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     datatable: {
       headers: {
-        colorScheme: {
-          dark: {
-            root: {
-              background: '#13131399',
-            },
-          },
+        root: {
+          background: '#13131399',
         },
       },
       row: {
-        colorScheme: {
-          dark: {
-            root: {
-              background: '#13131399',
-            },
-          },
+        root: {
+          background: '#13131399',
         },
       },
     },
     dialog: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     inputtext: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     menu: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     paginator: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: 'transparent',
-          },
-        },
+      root: {
+        background: 'transparent',
       },
     },
     select: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     selectbutton: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     textarea: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
     toast: {
-      colorScheme: {
-        dark: {
-          root: {
-            background: '#13131399',
-          },
-        },
+      root: {
+        background: '#13131399',
       },
     },
   },
