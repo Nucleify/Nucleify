cabaf395088dd44f110fac95dde616a22102ce1a
Kacper Bujak <kaccpper2@gmail.com>
2025-02-02 01:07:16 +0100

    ❓ Question: Pest.php fix

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,31 +1,31 @@
 <?php
 
 // // USE THIS IF YOU RUN TESTS ON LINUX
-// require_once __DIR__ . '/TestExpectations.php';
-// require_once __DIR__ . '/TestFunctions.php';
-// require_once __DIR__ . '/TestGroups.php';
-// require_once __DIR__ . '/TestUses.php';
+require_once __DIR__ . '/TestExpectations.php';
+require_once __DIR__ . '/TestFunctions.php';
+require_once __DIR__ . '/TestGroups.php';
+require_once __DIR__ . '/TestUses.php';
 
-// $articleData = require_once __DIR__ . '/TestConstants.php';
-// $updatedArticleData = require_once __DIR__ . '/TestConstants.php';
-// $contactData = require_once __DIR__ . '/TestConstants.php';
-// $updatedContactData = require_once __DIR__ . '/TestConstants.php';
-// $userData = require_once __DIR__ . '/TestConstants.php';
-// $updatedUserData = require_once __DIR__ . '/TestConstants.php';
-// $questionData = require_once __DIR__ . '/TestConstants.php';
-// $updatedQuestionData = require_once __DIR__ . '/TestConstants.php';
+$articleData = require_once __DIR__ . '/TestConstants.php';
+$updatedArticleData = require_once __DIR__ . '/TestConstants.php';
+$contactData = require_once __DIR__ . '/TestConstants.php';
+$updatedContactData = require_once __DIR__ . '/TestConstants.php';
+$userData = require_once __DIR__ . '/TestConstants.php';
+$updatedUserData = require_once __DIR__ . '/TestConstants.php';
+$questionData = require_once __DIR__ . '/TestConstants.php';
+$updatedQuestionData = require_once __DIR__ . '/TestConstants.php';
 
 // USE THIS IF YOU RUN TESTS ON WINDOWS
-require_once __DIR__ . '\TestExpectations.php';
-require_once __DIR__ . '\TestFunctions.php';
-require_once __DIR__ . '\TestGroups.php';
-require_once __DIR__ . '\TestUses.php';
+// require_once __DIR__ . '\TestExpectations.php';
+// require_once __DIR__ . '\TestFunctions.php';
+// require_once __DIR__ . '\TestGroups.php';
+// require_once __DIR__ . '\TestUses.php';
 
-$articleData = require_once __DIR__ . '\TestConstants.php';
-$updatedArticleData = require_once __DIR__ . '\TestConstants.php';
-$data = require_once __DIR__ . '\TestConstants.php';
-$updatedData = require_once __DIR__ . '\TestConstants.php';
-$userData = require_once __DIR__ . '\TestConstants.php';
-$updatedUserData = require_once __DIR__ . '\TestConstants.php';
-$questionData = require_once __DIR__ . '\TestConstants.php';
-$updatedQuestionData = require_once __DIR__ . '\TestConstants.php';
+// $articleData = require_once __DIR__ . '\TestConstants.php';
+// $updatedArticleData = require_once __DIR__ . '\TestConstants.php';
+// $data = require_once __DIR__ . '\TestConstants.php';
+// $updatedData = require_once __DIR__ . '\TestConstants.php';
+// $userData = require_once __DIR__ . '\TestConstants.php';
+// $updatedUserData = require_once __DIR__ . '\TestConstants.php';
+// $questionData = require_once __DIR__ . '\TestConstants.php';
+// $updatedQuestionData = require_once __DIR__ . '\TestConstants.php';
