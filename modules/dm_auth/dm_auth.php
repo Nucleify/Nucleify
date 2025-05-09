f16b432200dd30851eb38368daf000084d12e98e
SzymCode <szymon.radomski@yahoo.com>
2025-05-10 00:16:53 +0200

    Routes correction

--- modules/dm_auth/dm_auth.php
+++ modules/dm_auth/dm_auth.php
@@ -9,6 +9,5 @@ class dm_auth extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,18 @@ use Illuminate\Support\Facades\Route;
 /**
  *  Modules includes
  */
-// require base_path('modules/dm_activity/routes/web.php');
-// require base_path('modules/dm_auth/routes/web.php');
-// require base_path('modules/dm_entities/routes/web.php');
-// require base_path('modules/dm_entities_structural/routes/web.php');
-// require base_path('modules/dm_pages/routes/web.php');
+require base_path('modules/dm_activity/routes/web.php');
+require base_path('modules/dm_auth/routes/web.php');
+require base_path('modules/dm_entities/routes/web.php');
+require base_path('modules/dm_entities_structural/routes/web.php');
+require base_path('modules/dm_pages/routes/web.php');
 
 /**
  *  Logout
  */
-// Route::get('/logout', [LogoutController::class, 'logout'])
-//     ->middleware(['web', 'auth'])
-//     ->name('logout');
+Route::get('/logout', [LogoutController::class, 'logout'])
+    ->middleware(['web', 'auth'])
+    ->name('logout');
 
 /**
  *  Serve Nuxt payload
