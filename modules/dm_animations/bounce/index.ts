93cdbdbdfd2382866ece617a4d8e1d0060089fc6
SzymCode <szymon.radomski@yahoo.com>
2025-05-20 11:35:39 +0200

    Extracted bounce functions to their own files

--- modules/dm_animations/bounce/index.ts
+++ modules/dm_animations/bounce/index.ts
@@ -1,1 +1,3 @@
+export * from './utils'
+
 export { default as AnimationBounce } from './index.vue'
--- modules/dm_animations/bounce/index.vue
+++ modules/dm_animations/bounce/index.vue
@@ -3,42 +3,12 @@
 </template>
 
 <script setup lang="ts">
-import gsap from 'gsap'
+import { useBounceAnimation, useBounceClick } from '.'
 
 import AnimationLogo from './logo.vue'
 
 onMounted(() => {
-  gsap.set('svg', { opacity: 1 })
-
-  const ballBounce = gsap.to('.ball', {
-    keyframes: {
-      '0%': { y: -50, scaleX: 1, scaleY: 1 },
-      '7%': { y: -45, scaleY: 0.9, scaleX: 1.05, ease: 'sine.in' },
-      '25%': { y: 5, scaleY: 1.15, scaleX: 0.9, ease: 'sine.in' },
-      '50%': { y: 100, scaleX: 1, scaleY: 1, ease: 'none' },
-      '60%': { scaleX: 1.3, scaleY: 0.4, ease: 'none' },
-      '65%': { y: 150, scaleX: 1, scaleY: 1 },
-      '100%': { y: -50, scaleX: 1, scaleY: 1 },
-      easeEach: 'sine.out',
-    },
-    duration: 1.1,
-    repeat: -1,
-    transformOrigin: 'center bottom',
-  })
-
-  document.querySelector('.start-container').addEventListener('click', () => {
-    gsap.to(ballBounce, {
-      timeScale: 5,
-      duration: 0.2,
-      ease: 'power1.in',
-      onComplete: () => {
-        gsap.to(ballBounce, {
-          timeScale: 1,
-          duration: 0.25,
-          ease: 'power2.out',
-        })
-      },
-    })
-  })
+  const { ballBounce } = useBounceAnimation()
+  useBounceClick(ballBounce)
 })
 </script>
