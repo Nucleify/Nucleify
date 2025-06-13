745620051e192a7f45a5eb696253daae55a41985
K4mD4m <KamyszekDamian@outlook.com>
2025-06-13 17:06:48 +0200

    Molecule for MUI Backdrop

---
+++ next/src/app/atomic/molecule/backdrop/index.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "react";
+
+import Backdrop from "@mui/material/Backdrop";
+
+import { AdBackdropInterface } from "./types";
+
+export default function AdBackdrop({
+  children,
+  className = "",
+  sx,
+  ...rest
+}: AdBackdropInterface): JSX.Element {
+  return (
+    <Backdrop className={`ad-backdrop ${className}`} sx={sx} {...rest}>
+      {children}
+    </Backdrop>
+  );
+}
---
+++ next/src/app/atomic/molecule/backdrop/types/interfaces.ts
@@ -0,0 +1,18 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+export interface AdBackdropInterface {
+    open: boolean;
+    children?: React.ReactNode;
+    className?: string;
+    component?: React.ElementType;
+    components?: { Root?: React.ElementType };
+    componentsProps?: { root?: object };
+    invisible?: boolean;
+    slotProps?: {
+        root?: (() => void)
+        | object, transition?: (() => void)
+        | object
+    };
+    slots?: { root?: React.ElementType, transition?: React.ElementType };
+    sx?: SxProps<Theme>;
+}
