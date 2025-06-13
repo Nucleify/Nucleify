6624f63e932b460e166a9a3414f505944fac426d
K4mD4m <KamyszekDamian@outlook.com>
2025-06-13 19:35:00 +0200

    Molecule for MUI Breadcrumbs

---
+++ next/src/app/atomic/molecule/breadcrumbs/index.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "react";
+
+import { Breadcrumbs } from "@mui/material";
+
+import { AdBreadcrumbsInterface } from "./types";
+
+export default function AdBreadcrumbs({
+    children,
+    className = "",
+    sx,
+    ...rest
+}: AdBreadcrumbsInterface): JSX.Element {
+  return (
+    <Breadcrumbs className={`ad-breadcrumbs ${className}`} sx={sx} {...rest}>
+      {children}
+    </Breadcrumbs>
+  );
+}
---
+++ next/src/app/atomic/molecule/breadcrumbs/types/interfaces.ts
@@ -0,0 +1,18 @@
+import { SxProps, Theme } from '@mui/material';
+
+export interface AdBreadcrumbsInterface {
+    children?: React.ReactNode
+    className?: string
+    component?: React.ElementType
+    expandText?: string
+    itemsAfterCollapse?: number
+    itemsBeforeCollapse?: number
+    maxItems?: number
+    separator?: React.ReactNode
+    slotProps?: {
+        collapsedIcon?: (() => void)
+        | object
+    }
+    slots?: { CollapsedIcon?: React.ElementType }
+    sx?: SxProps<Theme>;
+}
