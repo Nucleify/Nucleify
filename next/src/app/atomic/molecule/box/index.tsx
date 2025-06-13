c1275db705a82445ebc1a8c536fb233a60279d44
K4mD4m <KamyszekDamian@outlook.com>
2025-06-13 19:16:20 +0200

    Molecule for MUI Box

---
+++ next/src/app/atomic/molecule/box/index.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "react";
+
+import { Box } from "@mui/material";
+
+import { AdBoxInterface } from "./types";
+
+export default function AdBox({
+    children,
+    component = "div",
+    sx,
+    ...rest
+    }: AdBoxInterface): JSX.Element {
+    return (
+        <Box component={component} sx={sx} {...rest}>
+        {children}
+        </Box>
+    );
+}
