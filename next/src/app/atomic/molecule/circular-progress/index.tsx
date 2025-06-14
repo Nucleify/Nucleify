657c7f3e6fbeaa1c8fe5b4fb60dc4e9b0451c319
K4mD4m <KamyszekDamian@outlook.com>
2025-06-14 11:52:19 +0200

    Molecule for MUI CircularProgress

---
+++ next/src/app/atomic/molecule/circular-progress/index.tsx
@@ -0,0 +1,19 @@
+import { JSX } from "react";
+
+import { CircularProgress } from "@mui/material";
+
+import { AdCircularProgressInterface } from "./types";
+
+export default function AdCircularProgress({
+    className = "",
+    sx,
+    ...rest
+}: AdCircularProgressInterface): JSX.Element {
+    return (
+        <CircularProgress
+            className={`ad-circular-progress ${className}`}
+            sx={sx}
+            {...rest}
+        />
+    );
+}
---
+++ next/src/app/atomic/molecule/circular-progress/types/interfaces.ts
@@ -0,0 +1,17 @@
+import type { SxProps, Theme } from '@mui/material';
+import type { OverridableStringUnion } from '@mui/types';
+import type { CircularProgressPropsColorOverrides } from '@mui/material/CircularProgress';
+
+export interface AdCircularProgressInterface {
+    className?: string
+    color?: OverridableStringUnion<
+        'inherit' | 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning',
+        CircularProgressPropsColorOverrides
+    >
+    disableShrink?: boolean
+    size?: number | string
+    sx?: SxProps<Theme>
+    thickness?: number
+    value?: number
+    variant?: 'determinate' | 'indeterminate'
+}
