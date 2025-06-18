28e40d2362c2b28a579cb493f3d550a8135c8465
K4mD4m <KamyszekDamian@outlook.com>
2025-06-18 12:19:48 +0200

    Corrections for MUI Accordion

--- next/src/app/atomic/molecule/accordion/types/interfaces.ts
+++ next/src/app/atomic/molecule/accordion/types/interfaces.ts
@@ -1,5 +1,27 @@
-import { AccordionProps } from '@mui/material/Accordion'
+import { SxProps, Theme } from "@mui/material"
+import { JSXElementConstructor } from "react"
+import { TransitionProps } from "@mui/material/transitions"
 
-export interface AdAccordionInterface extends AccordionProps {
+export interface AdAccordionInterface {
+  children: NonNullable<React.ReactNode>
   className?: string
+  defaultExpanded?: boolean
+  disabled?: boolean
+  disableGutters?: boolean
+  expanded?: boolean
+  onChange?: (event: React.SyntheticEvent, expanded: boolean) => void
+  slotProps?: {
+    heading?: object | ((ownerState: any) => object);
+    root?: object | ((ownerState: any) => object);
+    transition?: object | ((ownerState: any) => object);
+  }
+  slots?: {
+    heading?: React.ElementType;
+    root?: React.ElementType;
+    transition?: React.ElementType;
+  }
+  square?: boolean
+  sx?: SxProps<Theme>
+  TransitionComponent?: JSXElementConstructor<TransitionProps & { children?: React.ReactElement }>
+  TransitionProps?: object
 }
