ac2eddaacc1fb51a08664bf556ecb6091d5dfe92
SzymCode <szymon.radomski@yahoo.com>
2025-09-06 16:05:49 +0200

    dm_fields: passwordsMatch function corrections + tests

--- modules/dm_fields/utils/Check/passwords_match.ts
+++ modules/dm_fields/utils/Check/passwords_match.ts
@@ -2,5 +2,7 @@ export function passwordsMatch(
   password: string,
   password_confirmation: string
 ): boolean {
+  if (!password || !password_confirmation) return false
+
   return password === password_confirmation
 }
