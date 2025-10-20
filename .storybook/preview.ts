4ba56d368b871355e57e21f1f533ec57fca3e42a
SzymCode <szymon.radomski@yahoo.com>
2025-10-20 09:38:26 +0200

    Storybook configuration corrections

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,25 +1,16 @@
-/**
- *  Styles
- */
-import '../nuxt/atomic/boson/styles/index.scss'
+import '../nuxt/styles/index.scss'
 
-/**
- *  App mount
- */
 import { createApp } from 'vue'
 
-/**
- *  PrimeVue
- */
+import Lara from '@primeuix/themes/lara'
 import type { Preview } from '@storybook/vue3'
 import PrimeVue from 'primevue/config'
-import { DataManagerPreset } from '../nuxt/atomic/primevue_preset'
 
 export const app = createApp({})
 
 app.use(PrimeVue, {
   theme: {
-    preset: DataManagerPreset,
+    preset: Lara,
     options: {
       darkModeSelector: true,
     },
