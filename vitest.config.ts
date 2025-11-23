0af492ab989360703242766f47901b600221e3ab
SzymCode <szymon.radomski@yahoo.com>
2025-11-23 22:09:33 +0700

    Prepared setup file with mock functions for Vitest tests
    It speeds up tests nearly 2x times

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,11 +9,19 @@ export default defineVitestConfig({
   },
   test: {
     environment: 'nuxt',
+    setupFiles: ['./vitests/setup.ts'],
     include: [
       'vitests/**/*.{test,spec}.{js,ts,jsx,tsx}',
       'modules/**/*.{test,spec}.{js,ts,jsx,tsx}',
     ],
     testTimeout: 30000,
     onConsoleLog: () => false,
+    teardownTimeout: 5000,
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        singleThread: false,
+      },
+    },
   },
 })
