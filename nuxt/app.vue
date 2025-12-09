87e82f324ebd1865ddbafbebbe646183acd34a20
SzymCode <szymon.radomski@yahoo.com>
2025-12-09 21:12:12 +0100

    feat(meta): update meta title and description

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -17,19 +17,12 @@ import { resetColorsIfEmpty, useOfficeType } from 'atomic'
 const route = useRoute()
 
 useHead(() => ({
-  title:
-    'DataManager – Laravel/Nuxt ERP with Modular Design and Next-gen Architecture',
   htmlAttrs: {
     class: 'p-dark',
   },
   meta: [
     { property: 'og:image', content: '/img/og-image.png' },
     { property: 'og:logo', content: '/img/logo.svg' },
-    {
-      name: 'description',
-      content:
-        'Streamline ERP and design management with a powerful data manager built using Laravel and Nuxt. Easily organize, manage, and access all your data types in one powerful, user-friendly platform - perfect for CRM, ERP and E-commerce solutions.',
-    },
     {
       name: 'author',
       content: 'Szymon Radomski (SzymCode)',
