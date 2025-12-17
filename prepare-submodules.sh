f233eb2dfa70d75b8ab589df0b945873e85ae4ab
SzymCode <szymon.radomski@yahoo.com>
2025-12-18 00:36:58 +0100

    chore: add script to clone submodules

---
+++ prepare-submodules.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+echo "~~~ Running prepare-submodules script ~~~"
+
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+
+SUBMODULES="nuc_activity"
+
+for NAME in $SUBMODULES; do
+  URL="https://github.com/Nucleify/$NAME.git"
+  DIR="modules/$NAME"
+
+  echo "Cloning submodule $NAME from $URL into $DIR..."
+  git clone --depth=1 "$URL" "$DIR"
+done
+
+echo "Removing .git folders from submodules..."
+rm -rf modules/**/.git
+
+echo "~~~ prepare-submodules script finished ~~~"
