145344d9829a9b870b252911a0d3677f440fe688
SzymCode <szymon.radomski@yahoo.com>
2025-12-19 22:28:37 +0100

    chore(nuc_documentation): remove for git submodule setup

--- modules/nuc_documentation/nuc_documentation.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-
-import { NucDocumentationDashboard, NucDocumentationPage } from './atomic'
-
-export function registerNucDocumentation(app: App<Element>): void {
-  app
-    .component('nuc-documentation-page', NucDocumentationPage)
-    .component('nuc-documentation-dashboard', NucDocumentationDashboard)
-}
