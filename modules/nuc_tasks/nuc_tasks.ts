0cb80997b08a3180beab81f5b31c5a6239123c7b
SzymCode <szymon.radomski@yahoo.com>
2025-12-22 14:43:04 +0100

    chore(nuc_tasks): remove for git submodule setup

--- modules/nuc_tasks/nuc_tasks.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-
-import { NucTaskDashboard, NucTaskPage } from './atomic'
-
-export function registerNucTasks(app: App<Element>): void {
-  app
-    .component('nuc-task-page', NucTaskPage)
-    .component('nuc-task-dashboard', NucTaskDashboard)
-}
