668161618c07f0c77a6716e0f77d431b02f40a67
SzymCode <szymon.radomski@yahoo.com>
2025-12-22 14:39:19 +0100

    chore(nuc_stores): remove for git submodule setup

--- modules/nuc_stores/localStorage/get_item/index.ts
+++
@@ -1,7 +0,0 @@
-export function localStorageGetItem(item: string): string | undefined {
-  if (import.meta.client) {
-    return localStorage.getItem(item) || undefined
-  } else {
-    return undefined
-  }
-}
--- modules/nuc_stores/pinia/initial_store_state/keys.ts
+++
@@ -1,3 +0,0 @@
-import * as atomic from 'atomic'
-
-export const testStateKeys: atomic.StoreStateKeyType[] = ['one', 'two']
