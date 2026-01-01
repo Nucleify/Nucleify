15144fd9b9c9d2b7ff01e592ef1ce33dbd2712f3
SzymCode <szymon.radomski@yahoo.com>
2026-01-01 07:01:09 +0100

    chore(nuxt): disable layout transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,6 @@ export default defineNuxtConfig({
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' },
     head: {
       htmlAttrs: {
         lang: 'en',
