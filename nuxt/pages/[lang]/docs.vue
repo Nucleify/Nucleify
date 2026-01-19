533adc2bcfed165582c9fb80fd114e0e9afe7053
SzymCode <szymon.radomski@yahoo.com>
2026-01-19 08:56:48 +0100

    feat(docs): add multi-language support for documentation

---
+++ nuxt/pages/[lang]/docs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="documentation">
+    <nuc-documentation-page />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { isValidDocLang } from 'atomic'
+
+definePageMeta({
+  layout: 'documentation',
+  validate: (route) => {
+    const lang = route.params.lang as string
+    return isValidDocLang(lang) && lang !== 'en'
+  },
+})
+</script>
+
