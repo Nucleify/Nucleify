709dc8cdbc1ee9947e9e4c02614319cad8817b4b
SzymCode <szymon.radomski@yahoo.com>
2026-02-21 15:09:29 +0100

    refactor(home): delete navbar & footer to boost conversions

--- nuxt/layouts/front-office.vue
+++ nuxt/layouts/front-office.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="front-office">
-    <nuc-section-navbar />
+    <nuc-section-navbar v-if="!route.path.includes('/home')" />
     <main>
       <slot />
     </main>
-    <nuc-section-footer />
+    <nuc-section-footer v-if="!route.path.includes('/home')" />
   </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
+
 definePageMeta({
   layout: 'front-office',
 })
