1cfac036f0501480861fc71d41aa99012231aef1
SzymCode <szymon.radomski@yahoo.com>
2026-03-10 21:15:30 +0100

    chore(nuc_breadcrumb): add git submodule

--- nuxt/plugins/modules.ts
+++ nuxt/plugins/modules.ts
@@ -9,6 +9,7 @@ import {
   registerNucAdmin,
   registerNucAnimations,
   registerNucAuth,
+  registerNucBreadcrumb,
   registerNucCharts,
   registerNucColors,
   registerNucDarkMode,
@@ -48,6 +49,7 @@ export default defineNuxtPlugin({
     registerNucAdmin(nuxtApp.vueApp)
     registerNucAnimations(nuxtApp.vueApp)
     registerNucAuth(nuxtApp.vueApp)
+    registerNucBreadcrumb(nuxtApp.vueApp)
     registerNucCharts(nuxtApp.vueApp)
     registerNucColors(nuxtApp.vueApp)
     registerNucDarkMode(nuxtApp.vueApp)
