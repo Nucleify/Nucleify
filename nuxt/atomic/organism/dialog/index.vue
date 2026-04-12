a653964aa41643b566b16846d9117cc5a6aa58f4
SzymCode <szymon.radomski@yahoo.com>
2026-04-12 07:09:58 +0200

    fix: correct ad-dialog visibility handling and emit update event

--- nuxt/atomic/organism/dialog/index.vue
+++ nuxt/atomic/organism/dialog/index.vue
@@ -1,8 +1,10 @@
 <template>
   <Dialog
-    v-bind="transformProps(props)"
+    v-bind="transformProps(props, ['visible'])"
+    :visible="props.visible"
     :modal="props.modal || true"
     :show-header="props.showHeader || true"
+    :dismissable-mask="props.dismissableMask || true"
     :class="$style['ad-dialog']"
     :pt="{
       mask: $style['ad-dialog-mask'],
@@ -11,6 +13,7 @@
       content: $style['ad-dialog-content'],
       footer: $style['ad-dialog-footer'],
     }"
+    @update:visible="onVisibleUpdate"
   >
     <template #header> <slot name="header" /> </template>
 
@@ -26,6 +29,13 @@ import type { DialogInterface } from '.'
 import { transformProps } from '../../boson/transform_props'
 
 const props = defineProps<DialogInterface>()
+const emit = defineEmits<{
+  'update:visible': [value: boolean]
+}>()
+
+function onVisibleUpdate(value: boolean): void {
+  emit('update:visible', value)
+}
 </script>
 
 <style lang="scss" module>
